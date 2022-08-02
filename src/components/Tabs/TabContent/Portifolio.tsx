915174554f1f93e4ee53c4911b4dc7c8cd553537
Luiz Paulo @Enterprise <lindennerd@gmail.com>
2022-08-02 00:18:27 -0300

    portifolio projects added

--- src/components/Tabs/TabContent/Portifolio.tsx
+++ src/components/Tabs/TabContent/Portifolio.tsx
@@ -1,10 +1,55 @@
-import { Repository } from "@/graphql/graphql-operations";
+import Card from "@/components/Card";
+import { Maybe, Repository } from "@/graphql/graphql-operations";
+import Link from "next/link";
+import { RiGitRepositoryCommitsLine } from "react-icons/ri";
+import { BsLink45Deg } from "react-icons/bs";
+import { MdOutlineUpdate } from "react-icons/md";
+import moment from "moment";
 
 export function Portifolio({ repositories }: { repositories: Repository[] }) {
+  function formatDate(date: Maybe<string> | undefined) {
+    return moment(date).format("MM/DD/YYYY");
+  }
+
   return (
-    <div>
+    <div className="flex justify-between flex-wrap">
       {repositories.map((repository) => {
-        return <div>{repository.description}</div>;
+        return (
+          <div className="mt-2 mr-2 mb-2 w-full md:w-[30%]" key={repository.id}>
+            <Card>
+              <div
+                className="flex items-center px-2 rounded-md bg-purple-600 shadow-md dark:bg-gray-800 
+              text-white hover:bg-purple-700 dark:hover:bg-gray-700 transition-all"
+              >
+                <RiGitRepositoryCommitsLine />
+                <div className="p-1"></div>
+                <Link href={repository.url ?? ""} target="_blank">
+                  <a target="_blank">{repository.name}</a>
+                </Link>
+              </div>
+              <div className="px-2 mt-2 border-l-2 dark:text-white">
+                {repository.description}
+              </div>
+              <div className="flex  justify-between items-center  mt-8">
+                <div className="flex items-center">
+                  <MdOutlineUpdate className="text-xl" />
+                  <span className="ml-2">
+                    {formatDate(repository.updatedAt)}
+                  </span>
+                </div>
+                <Link href={repository.homepageUrl ?? ""} target="_blank">
+                  <a
+                    target="_blank"
+                    className="p-1 rounded-full bg-purple-600 
+                  shadow-md dark:bg-gray-800 text-white hover:bg-purple-700 dark:hover:bg-gray-700 transition-all"
+                  >
+                    <BsLink45Deg className="text-xl" />
+                  </a>
+                </Link>
+              </div>
+            </Card>
+          </div>
+        );
       })}
     </div>
   );
